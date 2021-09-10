22d9e43cf4de3cfea61614a6a6b0953f4530abf0
Hu4k3n <arjun.syam23@gmail.com>
2021-09-11 02:17:32 +0530

    final i think

---
+++ js/javaCourse.js
@@ -0,0 +1,26 @@
+const courseData = {
+    name: 'HTML',
+    resources: [
+        {
+            name: 'Lecture 1',
+            category: 'short',
+            yt: 'RRubcjpTkks',
+            duration: '4:48',
+            desc: 'Java tutorial 1'
+        },
+        {
+            name: 'Lecture 2',
+            category: 'medium',
+            yt: 'UmnCZ7-9yDY',
+            duration: '11:57',
+            desc:'Java tutorial 2'
+        },
+        {
+            name: 'Lecture 3',
+            category: 'long',
+            yt: 'xk4_1vDrzzo',
+            duration: '6:18:37',
+            desc:'Java Complete Tutorial'
+        },
+    ],
+};
---
+++ js/jsCourse.js
@@ -0,0 +1,26 @@
+const courseData = {
+    name: 'HTML',
+    resources: [
+        {
+            name: 'Lecture 1',
+            category: 'short',
+            yt: 'DHvZLI7Db8E',
+            duration: '4:48',
+            desc: 'JS tutorial 1'
+        },
+        {
+            name: 'Lecture 2',
+            category: 'medium',
+            yt: 'jx5jmI0UlXU',
+            duration: '11:57',
+            desc:'JS tutorial 2'
+        },
+        {
+            name: 'Lecture 3',
+            category: 'long',
+            yt: 'hKB-YGF14SY',
+            duration: '6:18:37',
+            desc:'JS Complete Tutorial'
+        },
+    ],
+};
