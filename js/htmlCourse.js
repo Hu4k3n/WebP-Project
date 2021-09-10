8cfb628982d8db9d36e9032c964adf631347b3d6
Anish Sharma <aneeshsharma@outlook.com>
2021-09-10 18:14:08 +0530

    implemented filters

---
+++ js/htmlCourse.js
@@ -0,0 +1,26 @@
+const courseData = {
+    name: 'HTML',
+    resources: [
+        {
+            name: 'Lecture 1',
+            category: 'short',
+            yt: 'JHq3pL4cdy4',
+            duration: '4:48',
+            desc: 'HTML Programming getting started'
+        },
+        {
+            name: 'Lecture 2',
+            category: 'medium',
+            yt: 'RjHflb-QgVc',
+            duration: '11:57',
+            desc:'More on HTML programming'
+        },
+        {
+            name: 'Lecture 3',
+            category: 'long',
+            yt: 'BsDoLVMnmZs',
+            duration: '3:11:11',
+            desc:'Very big HTML programming tutorial'
+        },
+    ],
+};
