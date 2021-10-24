676abcecb429899f00975f631d17e34ec3f2d305
HotMonkeyWings <devsony52@gmail.com>
2021-10-24 17:21:51 +0530

    Needs more polishing

--- index.php
+++ index.php
@@ -29,9 +29,29 @@
                     <i class="fa fa-close" style="position:relative;top:0px;left:0px;"></i>
                 </button>
             <h1> Hello there <?php echo $_SESSION['username'] ?>!</h1>
+                <table>
+                    <thead class="thead">
+                        <tr>
+                            <th class="th"> Registered Users </th>
+                        <tr>
+                    </thead>
+                    <tbody class="tbody">
+                        <?php 
+                            $user_list_query = "SELECT * FROM users";
+                            $result = mysqli_query($db, $user_list_query);
+                            $users = mysqli_fetch_array($result);
+
+                            while($row = mysqli_fetch_array($result))
+                            {
+                                echo "<tr class=\"tr\"><td>" . $row['username'] . "</td></tr>";
+                            }
+                            ?>
+                    </tbody>
+                </table>
             <div>
                 <button class="logout" onclick="window.location.href = 'logout.php';">Logout</button>
             </div>
+            
             </div>
             
             <?php endif; ?>
